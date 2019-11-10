ff640c164234c4074db309d75d05f264cff1b827
John Mutuma (Thoguo_) <johnmutuma5@gmail.com>
2019-11-10 11:10:32 +0300

    adjust plugins

--- create-sym-links.sh
+++ create-sym-links.sh
@@ -2,6 +2,7 @@ ln -s $(pwd)/datetime.vim ~/.vim/plugin/datetime.vim
 ln -s $(pwd)/system-clipboard.vim ~/.vim/plugin/system-clipboard.vim
 ln -s $(pwd)/custom-syntax.vim ~/.vim/plugin/custom-syntax.vim
 ln -s $(pwd)/toggle-colorscheme.vim ~/.vim/plugin/toggle-colorsheme.vim
+ln -s $(pwd)/dragvisuals.vim ~/.vim/plugin/dragvisuals.vim
 
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
