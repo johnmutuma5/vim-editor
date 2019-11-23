ef16759c8cb4f4a1f8e71de52889b0c97d50bc5f
John Mutuma (Thoguo_) <johnmutuma5@gmail.com>
2019-11-23 16:03:20 +0300

    add bash profile symlink

--- create-sym-links.sh
+++ create-sym-links.sh
@@ -1,8 +1,16 @@
+##  _          _ _       
+## | |__   ___| | | ___  
+## | '_ \ / _ \ | |/ _ \ 
+## | | | |  __/ | | (_) |
+## |_| |_|\___|_|_|\___/ 
+                      
 ln -s $(pwd)/datetime.vim ~/.vim/plugin/datetime.vim
 ln -s $(pwd)/system-clipboard.vim ~/.vim/plugin/system-clipboard.vim
 ln -s $(pwd)/custom-syntax.vim ~/.vim/plugin/custom-syntax.vim
 ln -s $(pwd)/toggle-colorscheme.vim ~/.vim/plugin/toggle-colorsheme.vim
 ln -s $(pwd)/dragvisuals.vim ~/.vim/plugin/dragvisuals.vim
 
+ln -s $(pwd)/.bash_profile ~/.bash_profile
+ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
