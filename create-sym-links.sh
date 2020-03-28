195d61fd8d55be9799a5a620e9b70d1e83efbe13
John Mutuma (Thoguo_) <johnmutuma5@gmail.com>
2020-03-28 13:10:56 +0300

    add coc files symlinks

--- create-sym-links.sh
+++ create-sym-links.sh
@@ -15,3 +15,9 @@ ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
+ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
+ln -s $(pwd)/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -s $(pwd)/salesforce-language-servers/node_modules/@salesforce/lwc-language-server/lib/server.js ~/.config/nvim/lwc-language-server.js
+ln -s $(pwd)/salesforce-language-servers/node_modules/@salesforce/aura-language-server/lib/server.js ~/.config/nvim/aura-language-server.js
+ln -s $(pwd)/salesforce-language-servers/apex-jorje-lsp.jar ~/.config/nvim/apex-jorje-lsp.jar
+
