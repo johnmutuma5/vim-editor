5d9fc8d8747632e391792ac4a55e8b52c0249c73
John Mutuma (Thoguo_) <johnmutuma5@gmail.com>
2020-03-28 19:47:30 +0300

    prefer symlic entire package on salesforce language servers
    - to make node modules available to aura and lwc servers

--- create-sym-links.sh
+++ create-sym-links.sh
@@ -17,7 +17,5 @@ ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
 ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
 ln -s $(pwd)/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -s $(pwd)/salesforce-language-servers/node_modules/@salesforce/lwc-language-server/lib/server.js ~/.config/nvim/lwc-language-server.js
-ln -s $(pwd)/salesforce-language-servers/node_modules/@salesforce/aura-language-server/lib/server.js ~/.config/nvim/aura-language-server.js
-ln -s $(pwd)/salesforce-language-servers/apex-jorje-lsp.jar ~/.config/nvim/apex-jorje-lsp.jar
+ln -s $(pwd)/salesforce-language-servers/ ~/.config/nvim/salesforce-language-servers
 
