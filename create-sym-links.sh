16063a3e761b33565fa2017f21a64240ceb6f8ce
John Mutuma <johnmutuma5@gmail.com>
2020-11-01 19:59:52 +0300

    force create symbolic links

--- create-sym-links.sh
+++ create-sym-links.sh
@@ -5,18 +5,18 @@
 ## |_| |_|\___|_|_|\___/ 
 
 # ln -s $(pwd)/.bash_profile ~/.bash_profile
-ln -s $(pwd)/.zshrc ~/.zshrc
-ln -s $(pwd)/.vimrc ~/.vimrc
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/.zshrc ~/.zshrc
+ln -sf $(pwd)/.vimrc ~/.vimrc
+ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
                       
-ln -s $(pwd)/datetime.vim ~/.vim/plugin/datetime.vim
-ln -s $(pwd)/system-clipboard.vim ~/.vim/plugin/system-clipboard.vim
-ln -s $(pwd)/custom-syntax.vim ~/.vim/plugin/custom-syntax.vim
-ln -s $(pwd)/toggle-colorscheme.vim ~/.vim/plugin/toggle-colorsheme.vim
-ln -s $(pwd)/color-devicons.vim ~/.vim/plugin/color-devicons.vim
+ln -sf $(pwd)/datetime.vim ~/.vim/plugin/datetime.vim
+ln -sf $(pwd)/system-clipboard.vim ~/.vim/plugin/system-clipboard.vim
+ln -sf $(pwd)/custom-syntax.vim ~/.vim/plugin/custom-syntax.vim
+ln -sf $(pwd)/toggle-colorscheme.vim ~/.vim/plugin/toggle-colorsheme.vim
+ln -sf $(pwd)/color-devicons.vim ~/.vim/plugin/color-devicons.vim
 # ln -s $(pwd)/dragvisuals.vim ~/.vim/plugin/dragvisuals.vim
 
-ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
-ln -s $(pwd)/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -s $(pwd)/pmd-apex.vim ~/.vim/plugged/ale/ale_linters/apexcode/pmd.vim
+ln -sf $(pwd)/init.vim ~/.config/nvim/init.vim
+ln -sf $(pwd)/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sf $(pwd)/pmd-apex.vim ~/.vim/plugged/ale/ale_linters/apexcode/pmd.vim
 
